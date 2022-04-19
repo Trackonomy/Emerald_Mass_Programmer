934adcb8bc2c472872ea7f412db4afe745b7d650
TanishGupta <tanish@trackonomysystems.com>
2022-04-19 09:29:14 -0700

    tested update 19_04_2022

--- TrkMassProg2.py
+++ TrkMassProg2.py
@@ -5,8 +5,7 @@ import time
 import os
 import subprocess
 import multiprocessing
-
-
+import inquirer
 from multiprocessing import Process, Manager
 from subprocess import Popen
 
@@ -14,6 +13,7 @@ macids = []
 qrCodes = []
 macqrpairs = {}
 flashed = False
+
 def scanQRcodes():
     global macids, qrCodes, macqrpairs
     # qrCodes = list(df['QR Code'])
@@ -31,7 +31,6 @@ def scanQRcodes():
             print("Duplicate QR detected, Please try again")
             qr = input("Scan Domino QR Code (enter 'q' when done scanning): ")
         qrCodes.append(qr)
-        print(len(qrCodes))
     print("Receiving data on each QR Code...")
     for qr in qrCodes:
         try:
@@ -64,72 +63,15 @@ def validateQR(qr):
 
     return False
 
+def getKeysByValue(dictOfElements, valueToFind):
+    listOfKeys = list()
+    listOfItems = dictOfElements.items()
+    for item  in listOfItems:
+        if item[1] == valueToFind:
+            listOfKeys.append(item[0])
+    return  listOfKeys
+# ======================================================================================================================
 
-def sendToArduino(sendStr):
-    ser.write(bytes(sendStr,'utf-8'))
-# ======================================
-
-# def recvFromArduino():
-#     global startMarker, endMarker
-#
-#     ck = ""
-#     x = "z"
-#     byteCount = -1
-#
-#     while ord(x) != startMarker:
-#         x = ser.read()
-#
-#     while ord(x) != endMarker:
-#         if ord(x) != startMarker:
-#             ck = ck + x.decode("utf-8")
-#             byteCount += 1
-#         x = ser.read()
-#
-#     return (ck)
-
-
-# ============================
-
-# def waitForArduino():
-#     global startMarker, endMarker
-#
-#     msg = ""
-#     while msg.find("Arduino is ready") == -1:
-#
-#         while ser.inWaiting() == 0:
-#             pass
-#
-#         msg = recvFromArduino()
-#
-#         print(msg)
-#         print()
-
-# def runTest(td):
-#     numLoops = len(td)
-#     waitingForReply = False
-#
-#     n = 0
-#     while n < numLoops:
-#         teststr = td[n]
-#
-#         if waitingForReply == False:
-#             sendToArduino(teststr)
-#             waitingForReply = True
-#         waitingForReply = False
-#         n += 1
-#
-#         if waitingForReply == True:
-#
-#             while ser.inWaiting() == 0:
-#                 pass
-#
-#             dataRecvd = recvFromArduino()
-#             print("Reply Received  " + dataRecvd)
-#             n += 1
-#             waitingForReply = False
-#
-#             print("===========")
-# ======================================
 def change_mac(mac, offset):
     return "{:012X}".format(int(mac, 16) + offset)
 
@@ -137,17 +79,17 @@ def change_mac(mac, offset):
 def parallelDfu(chx, zipf, com, macAddy,active=True):
     if active == True: #this is here to allow us an easy way of turning off a channel for testing
 
-        for i in range(5):
+        for i in range(1):
             print("firing: ", i)
             try:
                 os_cmd = 'nrfutil dfu ble -ic NRF52 -pkg ' + zipf + ' -p ' + com + ' -a ' + macAddy + ' -f -mtu 200'
                 if os.system(os_cmd) != 0:
                     if i == 4:
-                        raise Exception('Ch' + str(chx) + ' error ' )
+                        raise Exception('Ch' + str(chx) + ' error ')
                 else:
                     break
             except:
-                print("#####An exception occurred with Ch " + str(chx) )
+                print("#####An exception occurred with Ch " + str(chx))
 
 def runDFU():
     global flashed
@@ -155,8 +97,8 @@ def runDFU():
     com = ['COM9','COM10','COM12','COM13','COM14','COM15','COM16','COM18','COM19','COM20']
     # com = ['COM9','COM12','COM18','COM19','COM20']
     DFU_processes = {}
-    # fw = 'Em61x_MHM_LoRaWAN_V2.0.1_Final.zip'
-    fw = 'Bat_Test.zip'
+    fw = 'Em61x_MHM_LoRaWAN_V2.0.1_Final.zip'
+    # fw = 'Bat_Test.zip'
     for p in range(len(macids)):
         DFU_processes["p{0}".format(p)] = multiprocessing.Process(name="p{0}".format(p), target=parallelDfu, args=(p+1, fw, com[p], macAddyincr[p], True))
         (DFU_processes["p{0}".format(p)]).daemon = True
@@ -169,6 +111,9 @@ def runDFU():
 ####################################################################################################################################################################
 ####################################################################################################################################################################
 ####################################################################################################################################################################
+def sendToArduino(sendStr):
+    ser.write(bytes(sendStr, 'utf-8'))
+
 
 def run(start,finish,delaytime):
 
@@ -186,51 +131,75 @@ def run(start,finish,delaytime):
         print("Power off for node",y+1)
 
     print("-----------------------------")
-
-    endTime = round((time.time() - startTime), 2)
-    # print("Done...How did we do?")
+    # endTime = round((time.time() - startTime), 2)
+    # # print("Done...How did we do?")
+    # # print("")
+    # print(endTime, "seconds elapsed.")
     # print("")
-    print(endTime, "seconds elapsed.")
-    print("")
     # ser.close()
+
 if __name__ == '__main__':
-    macAddyincr = []
-    scanQRcodes()
-    for x in macids:
-        macAddyincr.append(change_mac(x, 1))
-    # print(macids)
-    # print(macAddyincr)
-    startTime = time.time()
+    counter = 1
+    while True:
+        macAddyincr = []
+        scanQRcodes()
+        for x in macids:
+            macAddyincr.append(change_mac(x, 1))
+        # print(macids)
+        # print(macAddyincr)
+        startTime = time.time()
 
-    os.system("cls")
-    print("/=======================================\\")
-    print("| Starting TRACKONOMY Mass programmer |")
-    print("========================================/")
-    print("| 22/04/15 v1.0.0, TG |")
-    print("\\=====================/")
-    print("")
-    numNodes = 10
+        os.system("cls")
+        print("/=======================================\\")
+        print("| Starting TRACKONOMY Mass programmer |")
+        print("========================================/")
+        print("| 22/04/15 v1.0.0, TG |")
+        print("\\=====================/")
+        print("")
+        numNodes = 10
 
-    serPort = "COM17"
-    baudRate = 9600
-    ser = serial.Serial(serPort, baudRate)
-    print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
+        serPort = "COM17"
+        baudRate = 9600
+        ser = serial.Serial(serPort, baudRate)
+        print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
 
-    # startMarker = 60
-    # endMarker = 62
-    # waitForArduino()
-    time.sleep(2)
-    sendToArduino(str(1))
-
-    run(0,numNodes,20)
-    # time.sleep(15)
-    runDFU()
-    x = ser.readline()
-    string_x = x.decode()
-    stripped_string_x = string_x.strip()
-    if stripped_string_x == "1" and flashed == True:
-        print("flashing complete.")
+        time.sleep(2)
+        # sendToArduino(str(len(macids)))
+        print('Putting Nodes into DFU mode')
         sendToArduino(str(1))
-        run(0, numNodes, 15)
-        ser.close()
+        run(0,numNodes,20)
+        runDFU()
+        x = ser.readline()
+        string_x = x.decode()
+        stripped_string_x = string_x.strip()
+        if stripped_string_x == "1" and flashed == True:
+            print("flashing complete.")
+            print('Sleeping Nodes')
+            sendToArduino(str(1))
+            run(0, numNodes, 15)
+            ser.close()
+        endTime = round((time.time() - startTime), 2)
 
+        print(endTime, "seconds elapsed.")
+        print("")
+        lines = ["Test # {}".format(counter), '# of Domino(s): {}'.format(len(macids)),'Time taken: {}'.format(endTime),'##################']
+        with open('Logging.txt', 'a') as f:
+            for line in lines:
+                f.write(line)
+                f.write('\n')
+        counter = counter + 1
+        macids = []
+        qrCodes = []
+        macqrpairs = {}
+        flashed = False
+        again_q = [
+            inquirer.List(
+                "Again",
+                message="Select Y to run again or N to exit",
+                choices=["Y", "N"],
+                default=["Y"],
+            ),
+        ]
+        again_a = inquirer.prompt(again_q)
+        if again_a['Again'] == "N":
+            break
